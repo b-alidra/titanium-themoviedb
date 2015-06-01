aca2d1b6d1f8772e55cb9ba52243a92aef8fbee8
Belkacem Alidra <belkacem.alidra@gmail.com>
2015-06-01 15:36:41 +0200

    Redesigned movie view

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,8 +1,13 @@
-var __  = require('alloy/underscore'),
-	api	= require('themoviedb/themoviedb');
+var underscore  = require('alloy/underscore'),
+	api			= require('themoviedb/themoviedb'),
+	t411		= new (require('t411/t411'))();
+	
+var TORRENTS_SLIDE_INDEX = 1;
+
 api.common.api_key = '1b3785a9a5de9fd3452af6e32e092357';
 
 var movie_id = arguments[0] || {};
+var movie;
 
 Alloy.Globals.loading.show(L('list_loading'), false);
 
@@ -13,53 +18,47 @@ $.torrentsTitle.text		= L('torrents');
 
 api.movies.getById({ 'id': movie_id, 'language': 'fr', 'append_to_response': 'images,list,credits', 'include_image_language': 'fr' },
 	function(response) {
-		if (__.isEmpty(response))
+		if (underscore.isEmpty(response))
 			return false;
 		
-		response = JSON.parse(response);
-		Ti.API.info(response);
-			
-		$.headerImage.image	= api.common.getImage({'size': 'w500', 'file': response.backdrop_path});
-		$.poster.image		= api.common.getImage({'size': 'w500', 'file': response.poster_path});
-		$.title.text		= response.title;
-		$.year.text			= response.release_date ? response.release_date.substring(0, 4) : '';
-		$.score.text		= response.popularity ? parseInt(response.popularity) + "%" : 0;
-		$.nbVotes.text		= response.vote_count ? response.vote_count : 0;
-		$.overview.value	= response.overview;
+		movie = JSON.parse(response);
+		
+		$.headerImage.image	= api.common.getImage({'size': 'w500', 'file': movie.backdrop_path});
+		$.poster.image		= api.common.getImage({'size': 'w500', 'file': movie.poster_path});
+		$.title.text		= movie.title;
+		$.year.text			= movie.release_date ? movie.release_date.substring(0, 4) : '';
+		$.score.text		= movie.popularity ? parseInt(movie.popularity) + "%" : 0;
+		$.nbVotes.text		= movie.vote_count ? movie.vote_count : 0;
+		$.overview.value	= movie.overview;
 		
 		$.cast_images.width = 0;
-		__.each(response.credits.cast, function(c) {
-			if (__.isEmpty(c["profile_path"]))
-				return false;
-			Ti.API.info(c);
-			$.cast_images.add(Ti.UI.createImageView({
-				"class": "cast_image",
-				"image" : api.common.getImage({'size': 'w500', 'file': c["profile_path"]}),
-				"width": "50dp",
-				"height": "80dp",
-				"right": "5dp",
-				"preventDefaultImage": true,
-				"borderRadius": 3,
-			}));
-			$.cast_images.width += 55;
-		});
 		
-		if (!__.isEmpty(response.genres)) {
+		if (!underscore.isEmpty(movie.genres)) {
 			var genres = [];
-			__.each(response.genres, function(g) {
+			underscore.each(movie.genres, function(g) {
 				genres.push(g.name);
 			});
 			$.genres.text = genres.join(' - ');
 		}
-		$.headerImage.image = api.common.getImage({'size': 'w500', 'file': response.backdrop_path});
+		$.headerImage.image = api.common.getImage({'size': 'w500', 'file': movie.backdrop_path});
 		
-		$.headerImage.addEventListener('load', function() {
-			$.wrapper.animate({
-				opacity : 1,
-				duration : 200
+		setTimeout(function() {
+			underscore.each(movie.credits.cast, function(c) {
+				if (underscore.isEmpty(c["profile_path"]))
+					return false;
+				$.cast_images.add(Ti.UI.createImageView({
+					"class": "cast_image",
+					"image" : api.common.getImage({'size': 'w500', 'file': c["profile_path"]}),
+					"width": "50dp",
+					"height": "80dp",
+					"right": "5dp",
+					"preventDefaultImage": true,
+					"borderRadius": 3,
+				}));
+				$.cast_images.width += 55;
 			});
-		});
-	
+		}, 1000);
+				
 		Alloy.Globals.loading.hide();
 	},
 	function() {
@@ -73,5 +72,24 @@ api.movies.getById({ 'id': movie_id, 'language': 'fr', 'append_to_response': 'im
 		return false;
 	}
 );
+
+$.tabs.addEventListener('scrollend', function(e) {
+	if (e.currentPage == TORRENTS_SLIDE_INDEX && !$.torrentsWrapper.hasLoadedTorrents) {
+		$.torrentsWrapper.hasLoadedTorrents = true;
+		Alloy.Globals.loading.show(L('list_loading'), false);
+		
+		t411.search({ term: movie.title, category: 631 }, function(err, response) {
+			if (err)
+				return false;
+				
+			var rows = [];
+			underscore.each(response.torrents, function(t) {
+				rows.push(Widget.createController('torrent', t).getView());
+			});
+			$.torrentsList.setData(rows);
+			Alloy.Globals.loading.hide(); 
+		});
+	}
+});
 
 $.backIcon.addEventListener('click', function() { $.win.close(); });
